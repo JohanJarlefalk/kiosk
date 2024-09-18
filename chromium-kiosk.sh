21d7ea33dab675aeebf51c64c0e6f7b97a067981
Johan Järlefalk <johan.jarlefalk@hotmail.com>
2024-09-18 17:02:44 +0200

    Update chromium-kiosk.sh

--- chromium-kiosk.sh
+++ chromium-kiosk.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Set DISPLAY environment variable
+export DISPLAY=:0
+
+# Optional: Set XAUTHORITY if your system requires it
+# export XAUTHORITY=/home/christ/.Xauthority
+
 # Path to Chromium executable
 CHROME=/usr/bin/chromium-browser
 
@@ -7,11 +13,32 @@ CHROME=/usr/bin/chromium-browser
 URL="http://localhost:8080/webvisu.htm"
 
 # Chromium flags
-FLAGS="--kiosk $URL --noerrdialogs --disable-infobars --disable-session-crashed-bubble --disable-restore-session-state --disable-prompt-on-repost --disable-extensions --disable-translate --disable-background-networking --disable-client-side-phishing-detection --disable-component-update --disable-default-apps --no-first-run --incognito --disable-gpu --fast --single-process"
+FLAGS="--kiosk $URL \
+       --noerrdialogs \
+       --disable-infobars \
+       --disable-session-crashed-bubble \
+       --disable-restore-session-state \
+       --disable-prompt-on-repost \
+       --disable-extensions \
+       --disable-translate \
+       --disable-background-networking \
+       --disable-client-side-phishing-detection \
+       --disable-component-update \
+       --disable-default-apps \
+       --no-first-run \
+       --incognito \
+       --disable-gpu \
+       --fast \
+       --single-process"
+
+# Log file (optional)
+LOGFILE=/home/christ/kiosk/chromium-kiosk.log
 
 # Loop to restart Chromium if it crashes
 while true
 do
-    $CHROME $FLAGS
+    echo "$(date): Starting Chromium" >> $LOGFILE
+    $CHROME $FLAGS >> $LOGFILE 2>&1
+    echo "$(date): Chromium crashed. Restarting in 2 seconds..." >> $LOGFILE
     sleep 2  # Wait before restarting
 done
